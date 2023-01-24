6d02afa155c53851c0d6205682d8cc8155f75656
Romel <romel_faustino@hotmail.com>
2023-01-23 22:53:20 -0500

    Agregando modelo compania y rol

---
+++ modelos/compania.modelo.php
@@ -0,0 +1,50 @@
+<?php
+    class Compania extends Conectar{
+        /* TODO:Listar Registros */
+        public function listarCompaniaSucursal(){
+            $conectar = parent::Conexion();
+            $mysql = "sp_ListaCompaniaSucursal";
+            $query = $conectar->prepare($mysql);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Listar Registro por ID en Especifico */
+        public function listarCompania($companiaid){
+            $conectar = parent::Conexion();
+            $mysql = "sp_ListaCompania ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $companiaid);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+       /*  TODO: Eliminar o cambiar estado a eliminado */
+        public function eliminarCompania($companiaid){
+            $conectar = parent::Conexion();
+            $mysql = "sp_EliminarCompania ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $companiaid);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Registro de Datos */
+        public function insertarCompania($nombrecompania){
+            $conectar = parent::Conexion();
+            $mysql = "sp_InsertarCompania ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $nombrecompania);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Actualizar Registro */
+        public function actualizarCompania($id,$nombrecompania){
+            $conectar = parent::Conexion();
+            $mysql = "sp_UpdateCompania ?,?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $id);
+            $query->bindParam(2, $nombrecompania);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
---
+++ modelos/rol.modelo.php
@@ -0,0 +1,53 @@
+<?php
+    class Rol extends Conectar{
+        /* TODO:Listar Registros */
+        public function listarRolSucursal($sucursalid){
+            $conectar = parent::Conexion();
+            $mysql = "sp_ListaRolSucursal ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $sucursalid);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Listar Registro por ID en Especifico */
+        public function listarRol($rolid){
+            $conectar = parent::Conexion();
+            $mysql = "sp_ListaRol ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $rolid);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+       /*  TODO: Eliminar o cambiar estado a eliminado */
+        public function eliminarRol($rolid){
+            $conectar = parent::Conexion();
+            $mysql = "sp_EliminarRol ?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $rolid);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Registro de Datos */
+        public function insertarRol($sucursalid,$nombrerol){
+            $conectar = parent::Conexion();
+            $mysql = "sp_InsertarRol ?,?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $sucursalid);
+            $query->bindParam(2, $nombrerol);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /* TODO: Actualizar Registro */
+        public function actualizarRol($id,$sucursalid,$nombrerol){
+            $conectar = parent::Conexion();
+            $mysql = "sp_UpdateRol ?,?,?";
+            $query = $conectar->prepare($mysql);
+            $query->bindParam(1, $id);
+            $query->bindParam(2, $sucursalid);
+            $query->bindParam(3, $nombrerol);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
